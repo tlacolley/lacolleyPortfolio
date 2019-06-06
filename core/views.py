5791fb49a922e17cf7befcce95ea5c071481dc5c
tlacolley <thomaslacolley@gmail.com>
2019-06-06 18:07:11 +0200

    Change portfolio part add context data and fixe some issues

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from django.template import RequestContext
 
 # Create your views here.
 from django.views.generic import TemplateView
-
+from lacolley_portfolio.models import WebWork, VFXWork
 
 from django.http import HttpResponse
 
@@ -13,7 +13,14 @@ from django.http import HttpResponse
 
 class IndexView(TemplateView):
     template_name = 'core/index.html'
+    model = VFXWork
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['compositing'] = VFXWork.objects.all().filter(category='comp')
+        print(context['compositing'])
 
-
-
+        context['matte'] = VFXWork.objects.all().filter(category='mat')
+        context['tracking'] = VFXWork.objects.all().filter(category='trk')
+        context['other'] = VFXWork.objects.all().filter(category='oth')
+        return context
--- lacolley_portfolio/static/lacolley_portfolio/script/script_portfolio.js
+++ lacolley_portfolio/static/lacolley_portfolio/script/script_portfolio.js
@@ -3,10 +3,19 @@ $(document).ready(function(){
 
   var topTabs = $(".topTabs");
   var tabsLinks = $(".topTabsLink");
+  var tabActive = $(".topTabsLink .active ")
 
+  // console.log(tabActive);  
+  console.log(tabsLinks);
+
+  
 
   tabsLinks.click(function(e) {
     e.preventDefault();
+    if(tabsLinks.hasClass("active")){
+      console.log("Actov");
+      
+    }
     topTabs.toggleClass("active");
 
   });
