229490536fec6df23c84d8623ce5263fa60abd46
tlacolley <thomaslacolley@gmail.com>
2019-06-17 19:39:54 +0200

    Change the Nuke logo, Change the preloading effect

--- core/static/lacolley_core/script/script_navbar.js
+++ core/static/lacolley_core/script/script_navbar.js
@@ -43,104 +43,118 @@ function displayEachIcon(){
   });
 }
 
+// Function for hide the icon list 
+function animHideList(){
+  titleNav.animate({opacity:0},400);
+  listNav.animate({opacity:0},400);
+};
+
+// Function for show, display the icon list 
+function displayList(){
+  labelList.show();
+  iconList.hide();
+  listNav.animate({opacity:1},700);
+  titleNav.animate({opacity:1},700);
+}
+
+//  Function to move the main to the center
+function changeMainCenter(){
+  main.toggleClass("mainSection");
+  main.toggleClass("mainSectionLeft");
+  main.animate({marginRight:"10%"},1000);
+}
+
+//  FUnction to change the Navbar position 
+function changeClassNavbar(){
+  navBar.toggleClass("navbarTop");
+  navBar.removeClass("navbarLeft");
+  // Change position main-------
+  setTimeout(changeMainCenter,1000);
+  // Change Icon by Liste ------
+  listNav.removeClass("ulLeft");
+  listNav.addClass("ulTop");
+  // Function display List ----
+  setTimeout(displayList,1500);
+
+}
+
+//  Function to Fixe the Navbar to the Top
+function animTopFixed(){
+  if(navBar.hasClass("navbarBase")||navBar.hasClass("navbarTop")){
+    // Change navbar class --------
+    navBar.removeClass("navbarBase");
+    navBar.removeClass("navbarTop");
+    // Add class Left ------------
+    navBar.toggleClass("navbarLeft");
+    // Animate for Main container-- 
+    setTimeout(changeMainRight,1000);
+    // Animation hide list --------
+    setTimeout(animHideList,750);
+    //  Change list by Icons ------
+    setTimeout(addClassUl,1900)
+    listNav.removeClass("ulTop");
+ } 
+}
+
+
+
+
+
 //  Condition for big Screen 
 
 
 function navbarScript(){
+  if (window.matchMedia('(min-width: 990px)').matches) {
 
-
-if (window.matchMedia('(min-width: 990px)').matches) {
-
-// Code Animation Navbar--------------------------------------------------------
-    var isFixed=false;
-    $(document).scroll(function () {
-        var topDist = $("header").height()-160;
-        // Set fixed
-        if( $(window).scrollTop()>=topDist && !isFixed ){
-          isFixed=true;
-          setTimeout(animTopFixed,0);
-          function animTopFixed(){
-            if(navBar.hasClass("navbarBase")||navBar.hasClass("navbarTop")){
-              // Change navbar class --------
-              navBar.removeClass("navbarBase");
-              navBar.removeClass("navbarTop");
-              // Add class Left ------------
-              navBar.toggleClass("navbarLeft");
-              // Animate for Main container-- 
-              setTimeout(changeMainRight,1000);
-              // Animation hide list --------
-              setTimeout(animHideList,750);
-              function animHideList(){
-                titleNav.animate({opacity:0},400);
-                listNav.animate({opacity:0},400);
-              };
-              //  Change list by Icons ------
-              setTimeout(addClassUl,1900)
-              listNav.removeClass("ulTop");
-           } 
+  // Code Animation Navbar--------------------------------------------------------
+      var isFixed=false;
+      $(document).scroll(function () {
+          var topDist = $("header").height()-160;
+          // Set fixed
+          if( $(window).scrollTop()>=topDist && !isFixed ){
+            isFixed=true;
+            setTimeout(animTopFixed,0);
+            
         }
-      }
-        // Set static
-        if( $(window).scrollTop()<topDist && isFixed ){
-          isFixed=false;
-          if(navBar.hasClass("navbarLeft") ){
+          // Set static
+          if( $(window).scrollTop()<topDist && isFixed ){
+            isFixed=false;
+            if(navBar.hasClass("navbarLeft") ){
 
-            // Hide list Icon --------------
-            listNav.animate({opacity:0},500);
-            titleNav.animate({opacity:0},500);  
+              // Hide list Icon --------------
+              listNav.animate({opacity:0},500);
+              titleNav.animate({opacity:0},500);  
 
-            // Change Nav position ---------
-            setTimeout(changeClassNavbar,500);
-            function changeClassNavbar(){
-              navBar.toggleClass("navbarTop");
-              navBar.removeClass("navbarLeft");
+              // Change Nav position ---------
+              setTimeout(changeClassNavbar,500);
 
-              // Change position main-------
-              setTimeout(changeMainCenter,1000);
-              function changeMainCenter(){
-                main.toggleClass("mainSection");
-                main.toggleClass("mainSectionLeft");
-                main.animate({marginRight:"10%"},1000);
-              }
-
-              // Change Icon by Liste ------
-              listNav.removeClass("ulLeft");
-              listNav.addClass("ulTop");
-
-              // Function display List ----
-              setTimeout(displayList,1500);
-              function displayList(){
-                labelList.show();
-                iconList.hide();
-                listNav.animate({opacity:1},700);
-                titleNav.animate({opacity:1},700);
-              }
             }
           }
-        }
-    });
+      });
 
-// End if for screen 
-} else {
-     var isFixed=false;
-    $(document).scroll(function () {
-        var topDist = $("header").height()-160;
-        // Set fixed
-        if( $(window).scrollTop()>=topDist && !isFixed ){
-          isFixed=true;
-          navBar.addClass("fixedNavbar");
+  // End if for screen 
+  } else {
+      var isFixed=false;
+      $(document).scroll(function () {
+          var topDist = $("header").height()-160;
+          // Set fixed
+          if( $(window).scrollTop()>=topDist && !isFixed ){
+            isFixed=true;
+            navBar.addClass("fixedNavbar");
+            }
+          // Set static
+          if( $(window).scrollTop()<topDist && isFixed ){
+            isFixed=false;
+            navBar.removeClass("fixedNavbar");
+
+
           }
-        // Set static
-        if( $(window).scrollTop()<topDist && isFixed ){
-          isFixed=false;
-          navBar.removeClass("fixedNavbar");
-
-
-        }
-    });
-}
+      });
+  }
 
 }
+
+
 
 navbarScript();
 $(window).resize(function(){
